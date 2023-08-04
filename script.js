bf952000c9b1f21c9afdef5b767341ddc6b81932
bghazarian <ghazarianberj98@gmail.com>
2023-08-04 20:03:04 +0400

    95%

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let form = document.querySelector("#form");
-let SendMsg = document.querySelector("#SendMsg");
+// toggle icon navbar
+let menuIcon = document.querySelector('.menu-icon');
+let navbar = document.querySelector('navbar');
+
+menuIcon.addEventListener('click', function(){
+    menuIcon.classList.toggle('bx-x');
+})
+
+
+// scroll sections
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 150);
+}
