7cd9168eec0af94035e7def9f7027b9704429cc2
Berj Ghazarian <99949283+Berj98@users.noreply.github.com>
2023-09-11 18:02:20 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('.menu-icon');
-let navbar = document.querySelector('navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener('click', function(){
-    menuIcon.classList.toggle('close');
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 })
 
 
@@ -31,4 +32,18 @@ window.onscroll = () => {
     // sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 150);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 }
+
+//animation text
+let typed = new Typed(".auto-typing", {
+    strings: ["Frontend Developer", "Backend Developer","UI Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
